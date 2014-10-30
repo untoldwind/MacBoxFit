f6a1f5f47ca496a4cb800cab7568213821214b45
Untoldwind <landru@untoldwind.net>
2014-10-30 19:10:05 +0100

    Initial Commit

---
+++ BoxFit/BoxFitView.h
@@ -0,0 +1,13 @@
+//
+//  BoxFitView.h
+//  BoxFit
+//
+//  Created by Bodo Junglas on 30.10.14.
+//  Copyright (c) 2014 Leanovate. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BoxFitView : ScreenSaverView
+
+@end
---
+++ BoxFit/BoxFitView.m
@@ -0,0 +1,52 @@
+//
+//  BoxFitView.m
+//  BoxFit
+//
+//  Created by Bodo Junglas on 30.10.14.
+//  Copyright (c) 2014 Leanovate. All rights reserved.
+//
+
+#import "BoxFitView.h"
+
+@implementation BoxFitView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
